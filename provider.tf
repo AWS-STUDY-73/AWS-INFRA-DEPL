820f4955a2d368bfb1cf859742024000659fce65
AWS-STUDY-73 <aws.study73@gmail.com>
2024-10-18 07:46:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.46"
+    }
+  }
+}
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
