c9647dc6ea16ec55375a0679c0f4875b5a980e90
AWS-STUDY-73 <aws.study73@gmail.com>
2024-10-18 08:47:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1"
-access_key = "$AWS_ACCESS_KEY_ID"
-secret_key = "$AWS_SECRET_ACCESS_KEY"
+access_key = "AKIA2UC3CDS2PUO4EPOS"
+secret_key = "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"
 }
